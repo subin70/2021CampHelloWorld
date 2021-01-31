b1896e6177cdd41c65b8a9966591fc2f2061a5c9
subin70 <21800339@handong.edu>
2021-01-31 23:48:43 +0900

    create spring project

---
+++ springCRUD/src/main/java/com/spring/bean/Emp.java
@@ -0,0 +1,42 @@
+package com.spring.bean;
+
+import java.sql.Date;
+
+public class Emp {
+	private int id;
+	private String name;
+	private float salary;
+	private String designation;
+	private Date regdate;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public float getSalary() {
+		return salary;
+	}
+	public void setSalary(float salary) {
+		this.salary = salary;
+	}
+	public String getDesignation() {
+		return designation;
+	}
+	public void setDesignation(String designation) {
+		this.designation = designation;
+	}
+	public Date getRegdate() {
+		return regdate;
+	}
+	public void setRegdate(Date regdate) {
+		this.regdate = regdate;
+	}
+}
